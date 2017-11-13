c39720673ed5caf82e5c51a27d140e09ed325c5a
Adam Dyszy <Adam Dyszy>
2017-11-13 22:53:03 +0100

    add creator checker

--- src/HelloWorldContract.sol
+++ src/HelloWorldContract.sol
@@ -3,15 +3,24 @@ pragma solidity ^0.4.0;
 
 contract HelloWorldContract {
     string private word;
-    function HelloWorldContract(){
+    address private creator;
+
+    function HelloWorldContract() public{
         word = "Hello World!";
+        creator=msg.sender;
     }
 
     function getWord1() public returns(string){//eats ether
+        if(msg.sender!=creator){
+            return "not creator!";
+        }
         return word;
     }
 
     function getWord2() public constant returns(string){//doesnt eat ether
+        if(msg.sender!=creator){
+            return "not creator!";
+        }
         return word;
     }
 
